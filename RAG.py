3367f7004ef6024623fa9909f42516202a3f156b
MapleTemple <113275690+MapleTemple@users.noreply.github.com>
2024-09-04 20:32:44 +0800

    Add files via upload

---
+++ RAG.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import load_dotenv, find_dotenv
+import langchain
+
+# 读取本地/项目的环境变量。
+
+# find_dotenv() 寻找并定位 .env 文件的路径
+# load_dotenv() 读取该 .env 文件，并将其中的环境变量加载到当前的运行环境中
+# 如果你设置的是全局的环境变量，这行代码则没有任何作用。
+_ = load_dotenv(find_dotenv())
